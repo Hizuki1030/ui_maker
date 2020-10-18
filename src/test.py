77965f327b1e40f7cd5ed1ed704a842b417843ee
Hizuki1030 <aibahizuki1030@gmail.com>
2020-10-18 18:36:38 +0900

    ignoreファイルの追加

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import database_manage
-
-a=database_manage.Database_manage(r"C:\Users\81909\Documents\python\ui_maker\動作確認用\databasetest\components_test.xml")
-for sample in a.generate_code("m5stack"):
-    print(sample)
