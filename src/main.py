79276e1784b404927b6bdda41df8a5127b0ee34f
Hizuki1030 <aibahizuki1030@gmail.com>
2020-10-19 12:59:55 +0900

    database_managerの機能追加

--- src/main.py
+++ src/main.py
@@ -14,16 +14,16 @@ class Uimaker(tkinter.Frame):
         pass
 
     def create_widgets(self):
-        self.draw_line_button = tkinter.Button(self, text='drawline', command=self.line_mode)#直線描画ツール
+        self.draw_line_button = tkinter.Button(self, text='drawline',command = self.drawLine)#直線描画ツール
         self.draw_line_button.grid(row=1, column=0)
 
-        self.draw_rectangle_button = tkinter.Button(self, text='draw rect', command=self.rectangle_mode)#長方形描画ツール（塗りつぶしなし）
+        self.draw_rectangle_button = tkinter.Button(self, text='draw rect',command = self.drawRectangle )#長方形描画ツール（塗りつぶしなし）
         self.draw_rectangle_button.grid(row=2, column=0)
 
-        self.clear_button = tkinter.Button(self, text='clear all', command=self.clear_canvas)#キャンバスリセット
+        self.clear_button = tkinter.Button(self, text='clear all',command= self.Canvas_reset)#キャンバスリセット
         self.clear_button.grid(row=0, column=1)
 
-        self.delete_object_button = tkinter.Button(self, text='delete_object', command=self.delete_object)#消去ボタン
+        self.delete_object_button = tkinter.Button(self, text='delete_object', command = self.Delete_componets)#消去ボタン
         self.delete_object_button.grid(row=0, column=2)
 
         self.line_width_label= tkinter.Label(self,text="太さ")
@@ -37,23 +37,35 @@ class Uimaker(tkinter.Frame):
         self.mouse_coordinate_label_text.set("x,y")
         self.mouse_coordinate_label=tkinter.Label(self,textvariable=self.mouse_coordinate_label_text)
         self.mouse_coordinate_label.grid(row=0, column=0)
-
+        object_tags=[]
         list_object_id_string=tkinter.StringVar(value=self)
-        object_tags=[x[0] for x in self.object_coordinate_datas]
+        #object_tags=[x[0] for x in self.object_coordinate_datas]
         self.object_list = tkinter.Listbox(self, listvariable=object_tags,width=28, height=15)
         self.object_list.grid(row=1,column=3,columnspan=2,rowspan=2)
 
         self.test_canvas = tkinter.Canvas(self, bg='white', width=320, height=240)
         print(self.test_canvas)
-        """self.test_canvas.grid(row=1, column=1, columnspan=2,rowspan=4)
-        self.test_canvas.bind('<B1-Motion>', self.draft)
+        self.test_canvas.grid(row=1, column=1, columnspan=2,rowspan=4)
+        """self.test_canvas.bind('<B1-Motion>', self.draft)
         self.test_canvas.bind('<B1-Motion>',self.mouse_coordinate_viewer,"+")
         self.test_canvas.bind('<ButtonRelease-1>', self.draw)
         self.test_canvas.bind('<Motion>',self.mouse_coordinate_viewer)
         self.object_list.bind('<Double-1>',  self.object_property)"""
 
-def line_mode(self):
-    print("")
+    def drawLine(self):
+        print("")
+
+    def drawRectangle(self):
+        return 0
+
+    
+
+    def Canvas_reset(self):
+        return 0
+
+    def Delete_componets(self):
+        return 0
+
 
 root = tkinter.Tk()
 app = Uimaker(master=root)
